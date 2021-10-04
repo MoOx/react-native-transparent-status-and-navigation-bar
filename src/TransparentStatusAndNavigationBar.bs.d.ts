65eb14d637bb831158d851d1f133d03c8f722b02
Hadrien Lanneau <hadrien@lanneau.me>
2021-10-04 16:10:17 +0200

    Add typescript types (#1)
    Co-authored-by: Hadrien Lanneau <hadrien.lanneau@gogowego.com>

---
+++ src/TransparentStatusAndNavigationBar.bs.d.ts
@@ -0,0 +1,7 @@
+import ReactNative from 'react-native'
+declare function init(): void;
+declare function setBarsStyle(
+  animatedOpt: boolean,
+  barsStyle: ReactNative.StatusBarStyle,
+): void;
+export { init, setBarsStyle };
