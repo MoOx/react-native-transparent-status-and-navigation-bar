1210c5a01b10aa86539bfd9e5a074d9fb4313417
Mathieu Acthernoene <zoontek@gmail.com>
2021-11-12 20:59:26 +0100

    Add nice buttons

--- example/App.tsx
+++ example/App.tsx
@@ -1,53 +1,104 @@
 import * as React from "react";
 import {
+  PlatformColor,
   Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
   useColorScheme,
+  View,
 } from "react-native";
 import {
+  NavigationBar,
   StatusBar,
   SystemBarStyle,
-  NavigationBar,
 } from "react-native-transparent-status-and-navigation-bar";
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     flex: 1,
+    alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#FFF",
+  },
+  button: {
+    padding: 16,
+    borderRadius: 4,
+    borderWidth: 1,
+  },
+  spacer: {
+    height: 20,
   },
 });
 
 export const App = () => {
   const isDarkMode = useColorScheme() === "dark";
 
-  const [barStyle, setBarStyle] =
-    React.useState<SystemBarStyle>("light-content");
+  const [statusBarStyle, setStatusBarStyle] = React.useState<SystemBarStyle>();
+  const [navigationBarStyle, setNavigationBarStyle] =
+    React.useState<SystemBarStyle>();
+
+  const buttonStyle = [
+    styles.button,
+    { borderColor: isDarkMode ? "white" : "black" },
+  ];
 
   return (
-    <SafeAreaView style={[styles.container]}>
-      <StatusBar barStyle={barStyle} />
-      <NavigationBar barStyle={barStyle} />
+    <SafeAreaView
+      style={[
+        styles.container,
+        { backgroundColor: isDarkMode ? "darkblue" : "pink" },
+      ]}
+    >
+      <StatusBar barStyle={statusBarStyle} />
+      <NavigationBar barStyle={navigationBarStyle} />
+
+      <Text>{"StatusBar barStyle: " + (statusBarStyle ?? "default")}</Text>
+
+      <View style={styles.spacer} />
 
       <Pressable
-        style={{ padding: 20 }}
-        onPress={() => {
-          setBarStyle("dark-content");
-        }}
+        accessibilityRole="button"
+        style={buttonStyle}
+        onPress={() => setStatusBarStyle("light-content")}
       >
-        <Text>dark-content</Text>
+        <Text>Set light StatusBar</Text>
       </Pressable>
 
+      <View style={styles.spacer} />
+
       <Pressable
-        style={{ padding: 20 }}
-        onPress={() => {
-          setBarStyle("light-content");
-        }}
+        accessibilityRole="button"
+        style={buttonStyle}
+        onPress={() => setStatusBarStyle("dark-content")}
       >
-        <Text>light-content</Text>
+        <Text>Set dark StatusBar</Text>
+      </Pressable>
+
+      <View style={styles.spacer} />
+      <View style={styles.spacer} />
+
+      <Text>
+        {"NavigationBar barStyle: " + (navigationBarStyle ?? "default")}
+      </Text>
+
+      <View style={styles.spacer} />
+
+      <Pressable
+        accessibilityRole="button"
+        style={buttonStyle}
+        onPress={() => setNavigationBarStyle("light-content")}
+      >
+        <Text>Set light NavigationBar</Text>
+      </Pressable>
+
+      <View style={styles.spacer} />
+
+      <Pressable
+        accessibilityRole="button"
+        style={buttonStyle}
+        onPress={() => setNavigationBarStyle("dark-content")}
+      >
+        <Text>Set dark NavigationBar</Text>
       </Pressable>
     </SafeAreaView>
   );
